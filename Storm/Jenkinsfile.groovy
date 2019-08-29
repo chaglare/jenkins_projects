747338de6b8d73c5cf9f2c2d6111e8e1c08d2001
Caglar Cavdar <chaglare@gmail.com>
2019-08-28 20:29:45 -0500

    script added

--- Storm/Jenkinsfile.groovy
+++ Storm/Jenkinsfile.groovy
@@ -9,9 +9,6 @@ node{
     stage("Remove Repo"){
         sh "ssh ec2-user@${ENVIR} sudo rm -rf /home/ec2-user/stormpath-flask-sample"
     }
-    stage("Remove PID"){
-        sh "ssh  ec2-user@${ENVIR} sudo kill $(sudo lsof -i:5000   | awk '{print $2}' | grep [[:digit:]]) 2> /dev/null"
-    }
     stage("Pull Repo"){
         sh "ssh ec2-user@${ENVIR} git clone https://github.com/chaglare/stormpath-flask-sample.git 2> /dev/null"
     }
